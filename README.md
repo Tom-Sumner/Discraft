2e160d1915b46776bc41cfb64c871700be1af035
Vasily <85069468+Edible223@users.noreply.github.com>
2022-04-06 16:08:20 +0100

    Uploaded Files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+  
 # Discraft
+
+Discraft lets you play a 2D version of Minecraft inside of discord!
+Discord uses [**Nextcord**](https://github.com/nextcord/nextcord) to create a user-freindly game with buttons to move your character and nicely formatted messages. Discraft also uses slash commands, so no remebering the bot prefix!
+Discraft uses custom world generation so you will never play the same map!
+
+## Example
+##### Just type /play and click enter!![Just type /play and click enter!](https://i.imgur.com/uYEcrxI.png)
+***
+##### The world generation will take a few seconds and then play!
+![The world generation will take a few seconds and then play!](https://i.imgur.com/79Ig6gd.png)
+***
+##### Use the nice buttons below the game to move your character!
+![Use the nice buttons below the game to move your character!](https://i.imgur.com/n1GOwHj.png)
+
+
+### Authors
+> [VasilyCodes](https://www.github.com/VasilyCodes)
+
+#### License
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
---
+++ discraft.py
@@ -0,0 +1,142 @@
+# Coded By: Tom Sumner
+# Date: 2020-04-24
+# Version: 0.12.1
+# Description: A simple bot that generates a world map.
+# Usage: python3 bot.py
+# Github: VasilyCodes
+# Discord: VasilyCodes#5215
+# License: MIT
+# Note: If you use this code, you MUST credit me in your project.
+
+
+import nextcord, time, os, random
+from random import randint
+from nextcord.ext import commands
+from nextcord import Interaction
+from nextcord import slash_command, SlashOption
+from dotenv import load_dotenv
+load_dotenv()
+
+def gen_y4():
+	g = "🟩"
+	s = "🟦"
+	result = []
+	for i in range(0, 19):
+		i = randint(0, 1)
+		if i == 0:
+			b = s
+		else:
+			b = g
+		result.append(b)
+	return result
+
+def gen_y3():
+	g = "🟩"
+	d = "🟫"
+	result = []
+	y4 = gen_y4()
+	for i in y4:
+		if i == g:
+			result.append(d)
+		else:
+			result.append(g)
+	return y4, result
+
+
+def gen_world():
+	y4, y3 = gen_y3()
+	y6 = ["🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦"]
+	y5 = ["🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦","🟦"]
+	y2 = ["🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫","🟫"]
+	world = f"{''.join(y6)}\n{''.join(y5)}\n{''.join(y4)}\n{''.join(y3)}\n{''.join(y2)}"
+	return world
+
+
+class World:
+	def __init__(self):
+		self.world = gen_world()
+		self.x = 0
+		self.y = 3
+
+	def move(self, x, y):
+		g = "🟩"
+		d = "🟫"
+		lvls = []
+		for lvl in range(0, 19):
+			lvls.append(self.world[lvl])
+		nextblock = lvls[y][x]
+		if x < 19 and x > -1:
+			pass
+		else:
+			raise Exception("Out of bounds")
+		if nextblock != g:
+			lvls[y][x] = "h"
+		self.world = f"{''.join(lvls[5])}\n{''.join(lvls[4])}\n{''.join(lvls[3])}\n{''.join(lvls[2])}\n{''.join(lvls[1])}\n{''.join(lvls[0])}"
+		return self.world
+
+class game:
+	world = World()
+	embed = nextcord.Embed(title="Minecraft", description="You can play minecraft in Discord with this bot!")
+	embed.add_field(name="Game", value=world.world.replace("h", "<:steve:960580473699057694>"))
+
+	class controls(nextcord.ui.View):
+		def __init__(self, user):
+			super().__init__(timeout=None)
+			self.user = user
+
+		@nextcord.ui.button(label="Backward", style=nextcord.ButtonStyle.green)
+		async def backward(self, button, ctx: Interaction):
+			if ctx.user != self.user:
+				await ctx.send(ephemeral=True, content="This is not your game to play!")
+			else:
+				pass
+			try:
+				world = game.world.move(game.world.x-1, 3)
+				embed = nextcord.Embed(title="Minecraft", description=f"{self.user.name}'s world")
+				embed.add_field(name="Game", value=world.replace("h", "<:steve:960580473699057694>"))
+				await ctx.message.edit(embed=embed)
+			except Exception("Out of bounds") as e:
+				await ctx.send(ephemeral=True, content="You can't go back!")
+
+		@nextcord.ui.button(label="Forward", style=nextcord.ButtonStyle.blurple)
+		async def forward(self, button, ctx: Interaction):
+			if ctx.user != self.user:
+				await ctx.send(ephemeral=True, content="This is not your game to play!")
+			else:
+				pass
+			try:
+				world = game.world.move(game.world.x+1, 3)
+				embed = nextcord.Embed(title="Minecraft", description=f"{self.user.name}'s world")
+				embed.add_field(name="Game", value=world.replace("h", "<:steve:960580473699057694>"))
+				await ctx.message.edit(embed=embed)
+			except Exception("Out of bounds") as e:
+				await ctx.send(ephemeral=True, content="You can't go forward!")
+
+
+
+client: nextcord.Client = commands.Bot(command_prefix=";", intents=nextcord.Intents.all())
+
+
+@client.event
+async def on_ready():
+	os.system("cls")
+	print("Bot is ready!")
+	time.sleep(1)
+	print("\r")
+	print(f"Logged in as {client.user}")
+
+
+
+@client.slash_command(name="play", description="Play a game of minecraft in Discord!", guild_ids=[932374983001378898, 936999558565744640])
+async def play(ctx: Interaction):
+	await ctx.send(ephemeral=True, content="Your game is being generated now! please wait...")
+	await ctx.channel.send(embed=game.embed, view=game.controls(user=ctx.user))
+	
+@client.slash_command("gen-world", description="Generate a world of minecraft in Discord!", guild_ids=[932374983001378898, 936999558565744640])
+async def gen(ctx: Interaction):
+	await ctx.send(ephemeral=True, content="Generating a world now! please wait...")
+	embed = nextcord.Embed(title="Minecraft", description="Generating a world...")
+	embed.add_field(name="Game", value=gen_world())
+	await ctx.channel.send(embed=embed)
+
+client.run(os.getenv("TOKEN"))
